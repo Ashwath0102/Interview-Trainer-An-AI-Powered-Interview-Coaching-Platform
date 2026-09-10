05c9d7f5fc093894dd234c916afa9885a0b19dab
Ashwath Baskar <cool.ashwath99@gmail.com>
2026-09-10 22:23:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: require("path").join(__dirname, "../.env") });
 // Inline defaults for development (overridden by .env)
-process.env.IBM_API_KEY = process.env.IBM_API_KEY || "rTUaVmUraoMVOg20k0glewNtHRMKo1EMLmdFzeXmXv98";
+process.env.IBM_API_KEY = process.env.IBM_API_KEY || "YOUR_API_KEY";
 process.env.IBM_ML_URL = process.env.IBM_ML_URL || "https://us-south.ml.cloud.ibm.com";
 process.env.IBM_PROJECT_ID = process.env.IBM_PROJECT_ID || "60b20a3f-d83b-442e-a387-a886d89212c6";
 process.env.IBM_MODEL_ID = process.env.IBM_MODEL_ID || "ibm/granite-4-h-small";
